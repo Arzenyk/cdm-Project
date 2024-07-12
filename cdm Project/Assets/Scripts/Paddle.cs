2684c178fae1a7b50e417937811891812de24209
Arzenyk <guillermitopereda@gmail.com>
2024-07-12 18:09:15 -0300

    Bounce Reflection

--- cdm Project/Assets/Scripts/Paddle.cs
+++ cdm Project/Assets/Scripts/Paddle.cs
@@ -13,6 +13,7 @@ public class Paddle : MonoBehaviour
     }
 
     public float speed = 30f;
+    public float maxbounceAngle = 75f;
 
     private void Awake()
     {
@@ -40,6 +41,28 @@ public class Paddle : MonoBehaviour
         if (this.direction != Vector2.zero)
         {
             this.rigidbody.AddForce(this.direction * this.speed);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+
+        if (ball != null)
+        {
+            Vector3 paddlePosition = this.transform.position;
+            Vector2 contactPoint = collision.GetContact(0).point;
+
+            float offset = paddlePosition.x - contactPoint.x;
+            float width = collision.otherCollider.bounds.size.x / 2;
+
+            float currentAngle = Vector2.SignedAngle(Vector2.up, ball.rigidbody.velocity);
+            float bounceAngle = (offset / width) * this.maxbounceAngle;
+            float newAngle = Mathf.Clamp(currentAngle + bounceAngle, -this.maxbounceAngle, this.maxbounceAngle);
+
+            Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+
+            ball.rigidbody.velocity = rotation * Vector2.up * ball.rigidbody.velocity.magnitude;
         }
     }
 }
